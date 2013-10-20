d554304a716919b7971b788428e9635072f73111
Aatash Parikh <aatash@berkeley.edu>
2013-10-20 16:37:42 -0700

    Failing step definitions for account/inventory tests

---
+++ features/step_definitions/account_steps.rb
@@ -0,0 +1,5 @@
+Then /I should see "(.*)"/ do |e1|
+  # Make sure that all the items in the app are visible in the table
+
+  flunk "Unimplemented"
+end
---
+++ features/step_definitions/inventory_steps.rb
@@ -0,0 +1,5 @@
+Then /I should see all the items/ do
+  # Make sure that all the items in the app are visible in the table
+
+  flunk "Unimplemented"
+end

fd7d0ee417f376ddeb839d530a8535cf4a3e3514
Joshua Lam <joh641@gmail.com>
2013-10-25 15:09:59 -0700

    migration for item image attribute

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
-  attr_accessible :category, :description, :legacy_id, :name, :quantity
+  attr_accessible :category, :description, :legacy_id, :name, :quantity, :image
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   has_many :reservations
   has_many :users, :through => :reservations
 end

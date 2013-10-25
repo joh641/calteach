0e9b3ac816abae0ba33ce5281c3959658e52f83e
Joshua Lam <joh641@gmail.com>
2013-10-25 14:59:40 -0700

    updated cucumber files

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Calteach::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :items
 
   # Sample resource route with options:
   #   resources :products do

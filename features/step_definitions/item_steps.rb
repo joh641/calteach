d7d345823318f0614d238c4762b53214843ced95
Yuxin Zhu <zhuyxn@gmail.com>
2013-10-27 20:46:16 -0700

    deleted function in item_steps"

--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -1,11 +1,3 @@
-Given /the following items exist/ do |items_table|
-  items_table.hashes.each do |item|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that item to the database here.
-    Item.create(item)
-  end
-end
-
 Then /I should see all the items that are in the database/ do
   Item.all.each do |item|
   	assert page.body.index(item.name) != nil

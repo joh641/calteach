9fc99e998c2db14e9ee3ad78d66982cd88174298
Joshua Lam <joh641@gmail.com>
2013-11-22 19:39:39 -0800

    fixed paths and controllers from admin namespace change

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,13 +1,72 @@
 class Admin::ReservationsController < ApplicationController
 
+  respond_to :html, :json
   before_filter :is_admin
 
   def index
     if params[:archived]
-      @reservations = Reservation.all
+      @reservation = Reservation.all
       @archived = true
     else
-      @reservations = Reservation.hide_archived
+      @reservation = Reservation.hide_archived
     end
   end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    @reservation.update_attributes(params[:reservation])
+    respond_with @reservation
+  end
+
+  def checkout
+    if params[:reserved]
+      reservation = Reservation.find_by_id(params[:id])
+      item = reservation.item
+      user = reservation.user
+    else
+      item = Item.find_by_id(params[:item])
+      reservation = Reservation.new
+      user = User.find_by_email(params[:email])
+      item.reservations << reservation if user
+      user.reservations << reservation if user
+    end
+    if user
+      reservation.date_out = Date.today
+      reservation.save
+      item.quantity -= 1
+      item.save
+      flash[:notice] = "Item #{item.name} was successfully checked out to #{user.name}"
+    else
+      flash[:warning] = "User does not exist. Please create an account for the user via the User Dashboard before checking out."
+    end
+    if params[:dashboard]
+      redirect_to admin_reservations_path
+    else
+      redirect_to item_path(item)
+    end
+  end
+
+  def checkin
+    reservation = Reservation.find_by_id(params[:id])
+    reservation.date_in = Date.today
+    reservation.save
+    item = reservation.item
+    item.quantity += 1
+    item.save
+    flash[:notice] = "Item #{item.name} was successfully checked in"
+    if params[:dashboard]
+      redirect_to admin_reservations_path
+    else
+      redirect_to item_path(item)
+    end
+  end
+
+  def archive
+    reservation = Reservation.find_by_id(params[:id])
+    reservation.archived = true
+    reservation.save
+    flash[:notice] = "Reservation was successfully archived"
+    redirect_to admin_reservations_path
+  end
+
 end

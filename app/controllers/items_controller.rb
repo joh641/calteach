d44cdec5b0f759119c5e6a6d59c4ba2831b9e681
Josh lam <joh641@gmail.com>
2013-11-25 13:17:56 -0800

    PG sort by ascending fix for item index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,10 +17,10 @@ class ItemsController < ApplicationController
 
   def index
     if params[:inactive]
-      @items = Item.inactive#.order(name: :asc)
+      @items = Item.inactive.find(:all, :order => "name ASC")
       @inactive = true
     else
-      @items = Item.active#.order(name: :asc)
+      @items = Item.active.find(:all, :order => "name ASC")
     end
 
     if params[:query]

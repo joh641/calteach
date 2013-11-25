fea5e84a93685ee20eafb485e03a7d1cfe65ce86
kgong <kevinkgong@gmail.com>
2013-11-24 21:23:31 -0800

    rspec mailer fix

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 
 describe UserMailer do
   describe "return_reminder" do
-    let(:mail) { UserMailer.return_reminder }
+    user = User.create(name: "John", email: "john@gmail.com", password: "password")
+    let(:mail) { UserMailer.return_reminder(user) }
 
     it "renders the headers" do
-      mail.subject.should eq("Return reminder")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Reservation Reminder")
+      mail.to.should eq(["john@gmail.com"])
+      mail.from.should eq(["lauraimai@calteach.com"])
     end
 
     it "renders the body" do

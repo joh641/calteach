14ee500a2b315b5b7412ec15fce2eb237e580e01
callmemc <callmemc@gmail.com>
2013-11-24 16:14:06 -0800

    removed unnecessary web step

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -144,14 +144,6 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
   end
 end
 
-Then /^(?:|I )should see ([^"]*) button$/ do |text|
-  if page.respond_to? :should
-    page.should have_content(text)
-  else
-    assert page.has_content?(text)
-  end
-end
-
 Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
   regexp = Regexp.new(regexp)
 

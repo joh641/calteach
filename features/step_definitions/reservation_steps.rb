0830d1972af37638562eb2e44e8ab4b17c1c8049
kgong <kevinkgong@gmail.com>
2013-11-24 16:32:45 -0800

    checkout resource features fixed

--- features/step_definitions/reservation_steps.rb
+++ features/step_definitions/reservation_steps.rb
@@ -10,7 +10,8 @@ Given /user has reserved "(.*?)"/ do |item|
   page.driver.submit :delete, "/users/sign_out", {}
   step %Q{I am logged into the user panel}
   step %Q{I am on the home page}
-  step %Q{I follow "#{item}"}
+  step %Q{I switch to List View}
+  step %Q{I follow "#{item}" within List View}
   step %Q{I fill in "reservation_start_date" with "11/16/2013"}
   step %Q{I fill in "reservation_end_date" with "11/26/2013"}
   step %Q{I press "Reserve"}

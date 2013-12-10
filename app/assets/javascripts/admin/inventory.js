59ed30d1a4f145be6719355cbdb571c4fff45741
Yuxin Zhu <zhuyxn@gmail.com>
2013-12-09 18:33:25 -0800

    removed legacy category js stuff in calteach

--- app/assets/javascripts/admin/inventory.js
+++ app/assets/javascripts/admin/inventory.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	// calteach.initInventory();
 	calteach.adjustInventoryColumnHeight();
     $(window).resize(function() {
         calteach.adjustInventoryColumnHeight();

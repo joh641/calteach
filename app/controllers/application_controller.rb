3fd5bf97b645eca14b209c5457039a3de1adcaed
Yuxin Zhu <zhuyxn@gmail.com>
2013-12-10 02:03:51 -0800

    boom 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,7 @@ class ApplicationController < ActionController::Base
     return true
   end
 
-  def redirect_and_flash(model, name, on)
-    if model == "User"
-      status = on ? "activated" : "deactivated"
-    elsif model == "Item"
-      status = on ? "archived" : "unarchived"
-    end
+  def redirect_and_flash(model, name, status)
     redirect_to :back, notice: "#{model} #{name} was successfully #{status}."
   end
 end

506063cdfb391c1b07050fc7eaeda37551db5cfd
Joshua Lam <joh641@gmail.com>
2013-12-10 06:55:12 -0800

    space for warning message

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -58,13 +58,13 @@ class ReservationsController < ApplicationController
     begin
       update_params(reservation, res_params, start_date, end_date)
     rescue
-      raise "Invalid Date"
+      raise " Invalid Date"
     end
   end
 
   def check_valid_reservation(start_date, end_date, reservation)
     if not Reservation.valid_reservation?(start_date, end_date, reservation.item, reservation.quantity, reservation, is_admin?)
-      raise "Conflicting Reservation"
+      raise " Conflicting Reservation"
     end
   end
 

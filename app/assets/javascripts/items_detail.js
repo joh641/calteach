7a32bb16321f20489b7d8634895ec42f40aa16b0
Yuxin Zhu <zhuyxn@gmail.com>
2013-12-09 23:47:20 -0800

    resolved js conflict with datepicker for reservations, added disappearing flashes after 4s

---
+++ app/assets/javascripts/items_detail.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var availability = $.parseJSON($('#reservation form').attr('avail_hash'));
+  var yesterday = new Date(new Date().setDate(new Date().getDate()-1));
+
+
+  var datepickerSettings = {
+      startDate: new Date(),
+      endDate: moment(new Date()).add('days', 59).toDate(),
+      autoclose: true,
+      beforeShowDay: function (date) {
+        var thisDate = moment(date).format("MM/DD/YYYY");
+        if (availability[thisDate] < $('#reservation_quantity').find(":selected").text()) {
+          return 'crossed';
+        } else {
+          if (moment(date) > moment(yesterday))
+          return 'bold';
+        }
+      }
+  };
+
+  $('#reservation .input-daterange').datepicker(datepickerSettings);
+  $('#reservation_quantity').change(function() {
+    $('#reservation .input-daterange').datepicker('remove');
+    $('#reservation_start_date').val('');
+    $('#reservation_end_date').val('');
+    $('#reservation .input-daterange').datepicker(datepickerSettings);
+  });
+});

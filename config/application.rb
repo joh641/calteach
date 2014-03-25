dda144b814e9e5a0d99330c2985063e1f6c82249
kgong <kevinkgong@gmail.com>
2014-03-24 20:25:28 -0700

    try again

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module Calteach
 
     # TODO Remove this in Rails 4.1
     # config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
-    config.secret_key_base = YAML.load(File.read("config/secret_token.yml"))
+    config.secret_key_base = YAML.load(File.read("config/secrets.yml"))
   end
 end
